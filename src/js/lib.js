ce3dd91a262095a146dbca6de428605814c6d9b3
aygulmardanova <mardanovaaigul@mail.ru>
2019-04-08 14:38:29 +0300

    const for length reserve value

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,11 +1,12 @@
 export function getWallPapersCount(lengthInM, widthInM, heightInM) {
     const wpWidthInM = 1.06;
     const wpLengthInM = 10;
+    const lengthReserveInM = 0.1;
 
     const perimeterInM = 2 * lengthInM + 2 * widthInM;
 
     let wpLines = perimeterInM / wpWidthInM;
-    let wpLinesInOneRoll = wpLengthInM / (heightInM + 0.1);
+    let wpLinesInOneRoll = wpLengthInM / (heightInM + lengthReserveInM);
 
     return wpLines / wpLinesInOneRoll;
 }
